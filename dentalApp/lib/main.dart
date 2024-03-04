f03aa43bbcf52c6356965143055b65998b8a6239
VasukiBhava <etest2280@gmail.com>
2024-03-04 23:18:54 +0530

    change app name

--- dentalApp/lib/main.dart
+++ dentalApp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'core/app_export.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
